ab30b627728bf14977ddb53ba7ac2fd8572ed9dd
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-05-09 16:25:41 -0300

    Adiciona SolarSystem e Missons Component

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
 import Title from './Title';
+import Planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {Planets.map((element) => (
+          <PlanetCard
+            key={ element.name }
+            planetName={ element.name }
+            planetImage={ element.image }
+          />
+        ))}
       </div>);
   }
 }
