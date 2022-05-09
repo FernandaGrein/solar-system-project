793607f31f702aafeb89119afac3c83c2646f061
Fernanda Grein <fernandagrein.fgm@outlook.com>
2022-05-09 16:52:36 -0300

    Adiciona MissionCard

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Title from './Title';
+import Missions from '../data/missions';
+import MissionCard from './MissionCard';
 
-class Missions extends React.Component {
+class Mission extends React.Component {
   render() {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
+        {Missions.map((item) => (
+          <MissionCard
+            key={ item.name }
+            name={ item.name }
+            year={ item.year }
+            country={ item.country }
+            destination={ item.destination }
+          />
+        ))}
       </div>);
   }
 }
 
-export default Missions;
+export default Mission;
